5047b58af1ff6c4f93fbecb08edb3b527d773826
Зимин Николай <Nikola.Zimin@x5.ru>
2023-05-23 21:43:41 +0300

    try delivery

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -9,6 +9,8 @@ sudo cp ./sausage-store.tar.gz /var/www-data/sausage-store.tar.gz
 #Распаковываем
 cd /var/www-data/
 sudo tar -xzvf sausage-store.tar.gz
+#Меняем владельцев
+sudo chown -R front-user /var/www-data/frontend
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
