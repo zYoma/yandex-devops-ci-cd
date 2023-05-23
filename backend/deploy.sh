bccb40e0e517b6ec08e3e21c970189ff1b920a46
Зимин Николай <Nikola.Zimin@x5.ru>
2023-05-23 21:43:41 +0300

    try delivery

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,6 +7,9 @@ sudo rm -f /var/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_BACKEND_URL}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /var/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+#Меняем владельцев
+sudo chown -R jarservice /var/jarservice
+sudo chgrp -R jarusers /var/jarservice
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
