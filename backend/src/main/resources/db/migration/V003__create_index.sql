a80ab475c6a857b95956ae2433ae9458ea79b4be
Зимин Николай <Nikola.Zimin@x5.ru>
2023-06-22 21:03:00 +0300

    debug mongo dsn

--- backend/src/main/resources/db/migration/V003__create_index.sql
+++ backend/src/main/resources/db/migration/V003__create_index.sql
@@ -1,1 +1,1 @@
-CREATE UNIQUE INDEX CONCURRENTLY idx_order_product_product_id_order_id ON order_product(product_id, order_id);
+CREATE INDEX IF NOT EXISTS idx_order_product_product_id_order_id ON order_product(product_id, order_id);
