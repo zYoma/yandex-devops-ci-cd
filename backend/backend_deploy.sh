52a08e3de5db6f6c326e89a31db34f7c34c81763
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-04 20:55:00 +0300

    try docker

---
+++ backend/backend_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +e
+cat > .env <<EOF
+SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
+SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
+SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
+SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
+EOF
+docker network create -d bridge sausage_network || true
+docker pull https://gitlab.praktikum-services.ru/std-017-003/sausage-store/sausage-backend:latest
+docker stop backend || true
+docker rm backend || true
+set -e
+docker run -d --name backend \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    --env-file .env \
+    https://gitlab.praktikum-services.ru/std-017-003/sausage-store/sausage-backend:latest
