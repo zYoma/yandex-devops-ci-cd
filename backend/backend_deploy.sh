16b9348d582a70f39e7fabf01c45a847ddea953d
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-05 17:37:17 +0300

    try

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,10 +6,10 @@ SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
-docker network create -d bridge sausage_network || true
+docker network create -d bridge sausage_network||true
 docker pull https://gitlab.praktikum-services.ru:5050/std-017-003/std-017-003/sausage-store/sausage-backend:latest
-docker stop backend || true
-docker rm backend || true
+docker stop backend||true
+docker rm backend||true
 set -e
 docker run -d --name backend \
     --network=sausage_network \
