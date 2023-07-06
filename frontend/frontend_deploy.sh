2bf7c4cceea8f500c23cd74003f2042ce8bb6177
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-06 13:06:44 +0300

    try

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set +e
-cat > .env <<EOF
-CURRENT_VERSION=${CURRENT_VERSION}
-VERSION=${VERSION}
-DEV_HOST=${DEV_HOST}
-EOF
 docker login -u ${CI_REGISTRY_USER} -p ${$CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-017-003/sausage-store/sausage-frontend:latest
@@ -15,5 +10,4 @@ docker run -d -p 80:80 --name frontend \
     --network=sausage_network \
     --restart always \
     --pull always \
-    --env-file .env \
     gitlab.praktikum-services.ru:5050/std-017-003/sausage-store/sausage-frontend:latest
