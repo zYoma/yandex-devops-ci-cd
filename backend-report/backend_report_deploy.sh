30f36ecf941b761f60af1bb00574c31955c4d982
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-18 21:29:37 +0300

    try docker-compose

--- backend-report/backend_report_deploy.sh
+++ backend-report/backend_report_deploy.sh
@@ -5,7 +5,8 @@ PORT=${PORT}
 DB=${DB}
 EOF
 docker login -u ${CI_REGISTRY_USER} -p ${$CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-
+docker stop sausage-store-backend-report || true
+docker rm sausage-store-backend-report || true
 set -e
 docker-compose up -d backend-report --build
 
