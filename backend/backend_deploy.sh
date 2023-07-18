2f4e4ce7a258447a0b392ef3a2e57093ea8c1719
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-18 20:44:02 +0300

    try docker-compose

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -7,9 +7,7 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
 docker login -u ${CI_REGISTRY_USER} -p ${$CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker network create -d bridge sausage_network || true
-docker pull gitlab.praktikum-services.ru:5050/std-017-003/sausage-store/sausage-backend:latest
 docker stop sausage-store-backend || true
 docker rm sausage-store-backend || true
 set -e
-docker-compose up -d sausage-store-backend --build
+docker-compose up -d backend --build
--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set +e
 docker login -u ${CI_REGISTRY_USER} -p ${$CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker network create -d bridge sausage_network || true
-docker pull gitlab.praktikum-services.ru:5050/std-017-003/sausage-store/sausage-frontend:latest
 docker stop sausage-store-frontend || true
 docker rm sausage-store-frontend || true
 set -e
-docker-compose up -d sausage-store-frontend --build
+docker-compose up -d frontend --build
