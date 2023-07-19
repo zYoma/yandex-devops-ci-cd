c097491dbf8efcd5f4a16df41d340edf1f376812
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-19 19:48:06 +0300

    try docker-compose

--- backend-report/backend_report_deploy.sh
+++ backend-report/backend_report_deploy.sh
@@ -4,7 +4,6 @@ cat > .env <<EOF
 PORT=8081
 DB=${DB}
 EOF
-docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker stop sausage-store-backend-report || true
 docker rm sausage-store-backend-report || true
 set -e
--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set +e
-docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker stop sausage-store-frontend || true
 docker rm sausage-store-frontend || true
 set -e
