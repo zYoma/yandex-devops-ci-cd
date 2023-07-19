05fb07213f99337542c635ab21a1ea9ca0344dba
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-19 16:10:17 +0300

    try docker-compose

--- backend-report/backend_report_deploy.sh
+++ backend-report/backend_report_deploy.sh
@@ -4,7 +4,7 @@ cat > .env <<EOF
 PORT=${PORT}
 DB=${MONGO_URI}
 EOF
-docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+
 docker stop sausage-store-backend-report || true
 docker rm sausage-store-backend-report || true
 set -e
