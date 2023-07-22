61e107f8d4fbb6f85e4a391226b10c7ea52f7d0d
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-22 06:09:39 +0300

    try nginx

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -4,7 +4,7 @@ cat > .env <<EOF
 SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
-SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
+SPRING_DATA_MONGODB_URI=mongodb://${MONGO_USER}:${MONGO_PASSWORD}@rc1a-u0nwp06gbwh8qsoq.mdb.yandexcloud.net:27018/${MONGO_USER}?tls=true&tlsAllowInvalidCertificates=true
 EOF
 docker stop sausage-store-backend || true
 docker rm sausage-store-backend || true
