ddd75b582223cb2207488caeacadf8655b78c5bc
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-23 16:13:58 +0300

    try blue

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,7 +6,8 @@ SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=mongodb://${MONGO_USER}:${MONGO_PASSWORD}@rc1a-u0nwp06gbwh8qsoq.mdb.yandexcloud.net:27018/${MONGO_USER}?tls=true&tlsAllowInvalidCertificates=true
 EOF
-docker stop sausage-store-backend || true
-docker rm sausage-store-backend || true
 set -e
-docker-compose up -d --build backend
+if [ "$( docker container inspect -f '{{.State.Running}}' 'blue' )" = "true" ];
+then docker stop green && docker-compose up -d --build green && docker stop blue;
+else docker stop blue && docker-compose up -d --build blue && docker stop green;
+fi
