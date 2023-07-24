74218374301bdda4cbaaac4637931c877be55c63
Зимин Николай <Nikola.Zimin@x5.ru>
2023-07-24 15:11:55 +0300

    try blue

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -8,6 +8,22 @@ SPRING_DATA_MONGODB_URI=mongodb://${MONGO_USER}:${MONGO_PASSWORD}@rc1a-u0nwp06gb
 EOF
 set -e
 if [ "$( docker container inspect -f '{{.State.Running}}' 'blue' )" = "true" ];
-then docker-compose up -d --build green && docker stop blue;
-else docker-compose up -d --build blue && docker stop green;
+then
+  if [ "$( docker container inspect -f '{{.State.Running}}' 'green' )" = "true" ];
+  then
+    docker stop green;
+  fi
+  until docker container ls --filter health=healthy | grep blue
+  do
+    sleep 5
+  done
+  echo "UP green"
+  docker-compose up -d green && docker stop blue;
+else
+  until docker container ls --filter health=healthy | grep green
+  do
+    sleep 5
+  done
+  docker-compose up -d blue && docker stop green;
+  echo "UP blue"
 fi
